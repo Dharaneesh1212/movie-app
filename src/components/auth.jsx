455d92007cf58f53eef70d2d63deffd5ada195a8
dharaneesh1212 <dharun12122002@gmail.com>
2024-04-02 12:39:55 +0530

    auth

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -17,27 +17,29 @@ const AuthContainer = () => {
           }`}
         >
           <form className="flex items-center justify-center flex-col gap-8 w-[25rem]">
-            <h1 className="text-2xl font-semibold font-mono">Create Account</h1>
+            <h1 className="animate__animated animate__zoomIn text-2xl font-semibold font-mono">
+              Create Account
+            </h1>
             <input
               type="text"
               placeholder="Name"
-              className="h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
+              className="animate__animated animate__zoomIn h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
             />
             <input
               type="email"
               placeholder="Email"
-              className="h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
+              className="animate__animated animate__zoomIn h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
             />
             <input
               type="password"
               placeholder="Password"
-              className="h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
+              className="animate__animated animate__zoomIn h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
             />
-            <button className="flex items-center justify-center h-10 w-[10rem] bg-purple-500 rounded-md font-sans font-medium text-lg">
+            <button className="animate__animated animate__zoomIn flex items-center justify-center h-10 w-[10rem] bg-gradient-to-r from-indigo-500 to-pink-400 hover:from-pink-500 hover:to-indigo-500 rounded-md font-sans font-medium text-lg">
               Sign Up
             </button>
             <button
-              className="flex items-center justify-center h-10 w-[10rem] bg-cyan-500 rounded-md font-mono font-medium text-xl"
+              className="animate__animated animate__zoomIn flex items-center justify-center h-10 w-[10rem] bg-cyan-500 rounded-md font-mono font-medium text-xl"
               onClick={handleTabToggle}
             >
               <FcGoogle />
@@ -50,18 +52,20 @@ const AuthContainer = () => {
           }`}
         >
           <form className="flex items-center justify-center flex-col gap-10 w-[25rem]">
-            <h1 className="text-2xl font-semibold font-mono">Sign In</h1>
+            <h1 className="animate__animated animate__zoomIn text-2xl font-semibold font-mono">
+              Sign In
+            </h1>
             <input
               type="email"
               placeholder="Email"
-              className="h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
+              className="animate__animated animate__zoomIn h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
             />
             <input
               type="password"
               placeholder="Password"
-              className="h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
+              className="animate__animated animate__zoomIn h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
             />
-            <button className="flex items-center justify-center h-10 w-[10rem] bg-fuchsia-500 rounded-md font-sans font-medium text-lg">
+            <button className="animate__animated animate__zoomIn flex items-center justify-center h-10 w-[10rem] bg-gradient-to-r from-pink-500 to-indigo-500 hover:from-indigo-500 hover:to-pink-400 rounded-md font-sans font-medium text-lg">
               Sign In
             </button>
           </form>
@@ -69,36 +73,36 @@ const AuthContainer = () => {
         <div>
           <div className="flex items-center justify-center flex-row">
             <div
-              className={`bg-purple-500 h-[30rem] w-[25rem] rounded-3xl flex justify-center items-center flex-col p-2 gap-4 ${
+              className={`bg-gradient-to-t from-indigo-500 via-purple-500 to-pink-500 h-[30rem] w-[25rem] rounded-3xl flex justify-center items-center flex-col p-2 gap-4 ${
                 activeTab === "sign-in" ? "" : "hidden"
               }`}
             >
-              <h1 className="text-3xl font-semibold font-mono">
+              <h1 className="animate__animated animate__zoomIn text-3xl font-semibold font-mono">
                 Welcome Back !
               </h1>
-              <p className="text-xl font-small font-mono">
+              <p className="animate__animated animate__zoomIn text-xl font-small font-mono">
                 Enter your personal details to use all site features
               </p>
               <button
-                className="flex items-center justify-center h-10 w-[8rem] bg-transparent rounded-md font-sans font-medium text-lg text-white shadow-[0_5px_15px_rgba(0,0,0,0.35)]"
+                className="animate__animated animate__zoomIn flex items-center justify-center h-10 w-[8rem] bg-transparent rounded-md font-sans font-medium text-lg text-white shadow-[0_5px_15px_rgba(0,0,0,0.35)]"
                 onClick={handleTabToggle}
               >
                 Sign Up
               </button>
             </div>
             <div
-              className={`bg-fuchsia-500 h-[30rem] w-[25rem] rounded-3xl flex justify-center items-center flex-col p-2 gap-4 ${
+              className={`bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 h-[30rem] w-[25rem] rounded-3xl flex justify-center items-center flex-col p-2 gap-4 ${
                 activeTab === "sign-up" ? "" : "hidden"
               }`}
             >
-              <h1 className="text-3xl font-semibold font-mono">
+              <h1 className="animate__animated animate__zoomIn text-3xl font-semibold font-mono">
                 Welcome Friend !
               </h1>
-              <p className="text-xl font-small font-mono">
+              <p className="animate__animated animate__zoomIn text-xl font-small font-mono">
                 Enter your personal details to use all site features
               </p>
               <button
-                className="flex items-center justify-center h-10 w-[8rem] bg-transparent rounded-md font-sans font-medium text-lg text-white shadow-[0_5px_15px_rgba(0,0,0,0.35)]"
+                className="animate__animated animate__zoomIn flex items-center justify-center h-10 w-[8rem] bg-transparent rounded-md font-sans font-medium text-lg text-white shadow-[0_5px_15px_rgba(0,0,0,0.35)]"
                 onClick={handleTabToggle}
               >
                 Sign In
