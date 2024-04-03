6582359faa74be64d51a0dcbd9f78ba679916dd8
dharaneesh1212 <dharun12122002@gmail.com>
2024-04-03 10:23:04 +0530

    carousel

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return ( 
-        <main>
-            <Link to="/">Home</Link>
-            <Link to="/watched">Watched</Link>
-            <Link to="/wishlist">Wishlist</Link>
-            <Link to="/auth">Signup</Link>
-        </main>
-     );
-}
- 
+  return (
+    <main>
+      <Link to="/">Home</Link>
+      <Link to="/watched">Watched</Link>
+      <Link to="/wishlist">Wishlist</Link>
+      <Link to="/auth">Signup</Link>
+    </main>
+  );
+};
+
 export default Navbar;
