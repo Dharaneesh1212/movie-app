dd79f86de2d71b3e6308f11e2e641f569700d4bf
dharaneesh1212 <dharun12122002@gmail.com>
2024-04-18 12:17:55 +0530

    api

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from "react";
 import Carousels from "./carousel";
 
 const Popular = () => {
+  const [popular, setPopular] = useState();
+  const url = "";
+
+  const getData = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setPopular(data);
+      console.log(data);
+    } catch (error) {
+      console.log("something went wrong", error);
+    }
+
+    useEffect(() => {
+      getData();
+    }, []);
+  };
   return (
     <main>
       <Carousels />
-      <h1>popular</h1>
+      <div></div>
     </main>
   );
 };
--- src/components/trending.jsx
+++ src/components/trending.jsx
@@ -1,6 +1,24 @@
+import { useState, useEffect } from "react";
 import Carousels from "./carousel";
 
 const Trending = () => {
+  const [trending, setTrending] = useState();
+  const url = "";
+
+  const getData = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setTrending(data);
+      console.log(data);
+    } catch (error) {
+      console.log("something went wrong", error);
+    }
+
+    useEffect(() => {
+      getData();
+    }, []);
+  };
   return (
     <main>
       <Carousels />
