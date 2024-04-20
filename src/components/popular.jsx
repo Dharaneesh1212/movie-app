9de88a6138c2eae0e4c33144014a9d0e80ac9681
dharaneesh1212 <dharun12122002@gmail.com>
2024-04-20 11:18:11 +0530

    popular

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Carousels from "./carousel";
 
 const Popular = () => {
-  // const [popular, setPopular] = useState();
+  const [popular, setPopular] = useState([]);
   const url =
     "https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=defd1902b4bb27bf2f154e42743a5266&page=1";
 
@@ -10,8 +10,8 @@ const Popular = () => {
     try {
       const res = await fetch(url);
       const data = await res.json();
-      // setPopular(data);
-      console.log(data);
+      setPopular(data.results);
+      console.log(data.results);
     } catch (error) {
       console.log("something went wrong", error);
     }
@@ -22,10 +22,45 @@ const Popular = () => {
   }, []);
 
   return (
-    <main>
-      <Carousels />
-      <div></div>
-    </main>
+    <main className="w-full">
+    <Carousels />
+      <h1 className="flex items-center justify-center m-4 text-2xl font-bold font-serif">POPULAR MOVIES</h1>
+    <div className="flex items-center justify-center flex-wrap">
+      {popular.map((movie) => (
+        <main
+          key={movie.id}
+          className="flex items-center justify-center flex-col h-[31rem] w-[25rem] gap-4 rounded-xl shadow-[0_5px_15px_rgba(0,0,0,0.35)] m-6"
+        >
+          <div>
+            <img
+              className="h-[18rem] w-[15rem]"
+              src={movie.poster_path}
+              alt="movie-img"
+            />
+          </div>
+          <div className="flex items-center justify-center flex-col gap-1">
+            <h1 className="flex items-center justify-center text-xl font-bold">
+              Title : {movie.title.slice(0, 20)}
+            </h1>
+            <span className="flex items-center justify-center font-semibold text-xl">
+              Language : {movie.original_language}
+            </span>
+            <p className="text-lg flex items-center justify-center">
+              Description : {movie.overview.slice(0, 30)}
+            </p>
+          </div>
+          <div className="flex items-center justify-center gap-8 text-xl font-semibold">
+            <button className="flex items-center justify-center gap-1 bg-green-600 text-white px-3 py-2 rounded-md">
+              Watched <FaCircleCheck />
+            </button>
+            <button className="flex items-center justify-center gap-1 bg-red-600 text-white px-3 py-2 rounded-md">
+              Wishlist <FaHeart />
+            </button>
+          </div>
+        </main>
+      ))}
+    </div>
+  </main>
   );
 };
 
