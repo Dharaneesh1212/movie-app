5b84aec2c964253a75160feb1c088cc28174eb3d
dharaneesh1212 <dharun12122002@gmail.com>
2024-04-21 12:15:42 +0530

    design

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -46,36 +46,41 @@ const Popular = () => {
       <div className="flex items-center justify-center flex-wrap">
         {popular.map((movie) => (
           <main
-            key={movie.id}
-            className="flex items-center justify-center flex-col h-[31rem] w-[25rem] gap-4 rounded-xl shadow-[0_5px_15px_rgba(0,0,0,0.35)] m-6"
-          >
-            <div>
-              <img
-                className="h-[18rem] w-[15rem]"
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt="movie-img"
-              />
-            </div>
-            <div className="flex items-center justify-center flex-col gap-1">
-              <h1 className="flex items-center justify-center text-xl font-bold">
-                Title : {movie.title.slice(0, 20)}
-              </h1>
-              <span className="flex items-center justify-center font-semibold text-xl">
-                Language : {movie.original_language}
-              </span>
-              <p className="text-lg flex items-center justify-center">
-                Description : {movie.overview.slice(0, 30)}
-              </p>
-            </div>
-            <div className="flex items-center justify-center gap-8 text-xl font-semibold">
-              <button className="flex items-center justify-center gap-1 bg-green-600 text-white px-3 py-2 rounded-md">
-                Watched <FaCircleCheck />
-              </button>
-              <button className="flex items-center justify-center gap-1 bg-red-600 text-white px-3 py-2 rounded-md">
-                Wishlist <FaHeart />
-              </button>
-            </div>
+          key={movie.id}
+          className="flex items-center justify-center flex-col h-[30rem] w-[38rem] gap-8 rounded-xl shadow-[0_5px_15px_rgba(0,0,0,0.35)] m-6"
+        >
+          <main className="flex items-center justify-center flex-row gap-4">
+          <div>
+            <img
+              className="h-[18rem] w-[15rem]"
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt="movie-img"
+            />
+          </div>
+          <div className="flex items-start justify-center flex-col gap-2 w-[18rem]">
+            <h1 className="flex items-center justify-center text-xl font-bold">
+              Title : {movie.title.slice(0, 20)}
+            </h1>
+            <span className="flex items-center justify-center font-semibold text-xl">
+              Language : {movie.original_language}
+            </span>
+            <p className="text-lg font-medium flex items-center justify-center ml-1 mr-1">
+              Description : {movie.overview.slice(0,250)}
+            </p>
+            <p className="font-medium text-lg flex items-center justify-center ml-1 mr-1">
+              Release date : {movie.release_date}
+            </p>
+          </div>
           </main>
+          <div className="flex items-center justify-center gap-8 text-xl font-semibold">
+            <button className="flex items-center justify-center gap-1 bg-green-600 text-white px-3 py-2 rounded-md">
+              Watched <FaCircleCheck />
+            </button>
+            <button className="flex items-center justify-center gap-1 bg-red-600 text-white px-3 py-2 rounded-md">
+              Wishlist <FaHeart />
+            </button>
+          </div>
+        </main>
         ))}
       </div>
     </main>
--- src/components/trending.jsx
+++ src/components/trending.jsx
@@ -47,8 +47,9 @@ const Trending = () => {
         {trending.map((movie) => (
           <main
             key={movie.id}
-            className="flex items-center justify-center flex-col h-[31rem] w-[25rem] gap-4 rounded-xl shadow-[0_5px_15px_rgba(0,0,0,0.35)] m-6"
+            className="flex items-center justify-center flex-col h-[30rem] w-[38rem] gap-8 rounded-xl shadow-[0_5px_15px_rgba(0,0,0,0.35)] m-6"
           >
+            <main className="flex items-center justify-center flex-row gap-4">
             <div>
               <img
                 className="h-[18rem] w-[15rem]"
@@ -56,17 +57,21 @@ const Trending = () => {
                 alt="movie-img"
               />
             </div>
-            <div className="flex items-center justify-center flex-col gap-1">
+            <div className="flex items-start justify-center flex-col gap-2 w-[18rem]">
               <h1 className="flex items-center justify-center text-xl font-bold">
                 Title : {movie.title.slice(0, 20)}
               </h1>
               <span className="flex items-center justify-center font-semibold text-xl">
                 Language : {movie.original_language}
               </span>
-              <p className="text-lg flex items-center justify-center ml-1 mr-1">
-                Description : {movie.overview.slice(0, 30)}
+              <p className="text-lg font-medium flex items-center justify-center ml-1 mr-1">
+                Description : {movie.overview.slice(0,250)}
+              </p>
+              <p className="font-medium text-lg flex items-center justify-center ml-1 mr-1">
+                Release date : {movie.release_date}
               </p>
             </div>
+            </main>
             <div className="flex items-center justify-center gap-8 text-xl font-semibold">
               <button className="flex items-center justify-center gap-1 bg-green-600 text-white px-3 py-2 rounded-md">
                 Watched <FaCircleCheck />
