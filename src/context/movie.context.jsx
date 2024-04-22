077ecce0216a023da7b500c78de162d4382c275f
dharaneesh1212 <dharun12122002@gmail.com>
2024-04-22 13:25:25 +0530

    new

--- src/context/movie.context.jsx
+++ src/context/movie.context.jsx
@@ -2,12 +2,20 @@ import { useState, createContext, useContext } from "react";
 
 const MovieContext = createContext();
 
-const MovieContextProvider = ({ children }) => {
-  const [add, setAdd] = useState();
-};
+const MovieContextProvider = ({children}) => {
+
+    const [add, setAdd] = useState()
+
+    return ( 
+        <MovieContext.Provider>
+            {children}
+        </MovieContext.Provider>
+     );
+}
+
 
 const usemovie = () => {
-    return useContext(MovieContext)
+  return useContext(MovieContext);
 };
 
 export { MovieContextProvider, usemovie };
