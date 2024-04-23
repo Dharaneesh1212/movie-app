45aea5a4021249093e6db5eeb89f28bfd9d19f49
dharaneesh1212 <dharun12122002@gmail.com>
2024-04-23 11:51:47 +0530

    toast

--- src/context/movie.context.jsx
+++ src/context/movie.context.jsx
@@ -1,36 +1,47 @@
 import { useState, createContext, useContext } from "react";
+// toast message
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MovieContext = createContext();
 
-const MovieContextProvider = ({children}) => {
+const MovieContextProvider = ({ children }) => {
+  const [watch, setWatch] = useState([]);
+  const [wish, setWish] = useState([]);
+  const [remove, setRemove] = useState([]);
 
-    const [watch, setWatch] = useState([])
-    const [wish, setWish] = useState([])
-
-    const addWatched=(movie)=>{
+  const addWatched = (movie) => {
+    const isAlready = watch.some((m) => m.id === movie.id);
+    if (isAlready) {
+      toast.error("Movie already exists in the watched list!");
+    } else {
       setWatch([...watch, movie]);
-    };
-    const addWishlist=(movie)=>{
+      toast.success("Movie added to watched successfully!");
+    }
+  };
+  const addWishlist = (movie) => {
+    const isAlready = wish.some((m) => m.id === movie.id);
+    if (isAlready) {
+      toast.error("Movie already exists in the wishlist!");
+    } else {
       setWish([...wish, movie]);
-    };
-    const removeMovie=()=>{
+      toast.success("Movie added to wishlist successfully!");
+    }
+  };
+  const removeMovie = () => {};
 
-    };
-
-    const values = {
-        watch,
-        wish,
-        addWatched,
-        addWishlist,
-        removeMovie
-      };
-    return ( 
-        <MovieContext.Provider value={values}>
-            {children}
-        </MovieContext.Provider>
-     );
+  const values = {
+    watch,
+    wish,
+    remove,
+    addWatched,
+    addWishlist,
+    removeMovie,
+  };
+  return (
+    <MovieContext.Provider value={values}>{children}</MovieContext.Provider>
+  );
 };
-
 
 const usemovie = () => {
   return useContext(MovieContext);
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { MovieContextProvider } from "./context/movie.context.jsx";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <MovieContextProvider>
       <App />
+      <ToastContainer />
     </MovieContextProvider>
 );
